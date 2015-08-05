823c4f96e4d61f65d68e89b483278f126c9ca29b
Fahlur <ti07shadow@gmail.com>
2015-08-05 04:38:05 -0500

    Update main.java

--- src/me/Fahlur/InactiveRegions/main.java
+++ src/me/Fahlur/InactiveRegions/main.java
@@ -23,12 +23,22 @@ import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class main extends JavaPlugin  {
 	
+	/*
+	 * ~~~ Developer Notes ~~~
+	 * + Need to make lists by command page index, say 5 items per page?, maybe 10?
+	 * + Need to make multi-owner checks a thing, make sure if any owner is active at all
+	 * + Need to make invalid owner command, report ALL invalid owners, no owner on main regions, 
+	 *   or invalid minecraft name, maybe a player never been on server name as well!
+	 * + Whitelist, including timed whitlist
+	 * 
+	 */
+	
 	Plugin plugin = this;
 	public static Essentials ess3 = null;
 	public static WorldGuardPlugin wg = null;
 	PluginDescriptionFile pdfFile = this.getDescription();
 	List<String> oNames = new ArrayList<String>();
-	
+	List<String> invalidNames = new ArrayList<String>();
 	
 	@Override
 	public void onEnable(){
@@ -77,6 +87,42 @@ public class main extends JavaPlugin  {
 			player.sendMessage(ChatColor.GOLD + "Description: " + ChatColor.WHITE + pdfFile.getDescription());
 		}
 	
+		if(cmd.getName().equalsIgnoreCase("invalidregions")){
+			
+			int i = 0;
+			RegionManager worldGuard = wg.getRegionManager(player.getWorld());
+			Map<String, ProtectedRegion> regionList = worldGuard.getRegions();
+			
+			
+			for(Entry<String, ProtectedRegion> list : regionList.entrySet()){
+				String newList = list.getValue().getId();
+				if (!newList.equalsIgnoreCase("__global__")){
+	
+					String owners = list.getValue().getOwners().toPlayersString().trim().replace("uuid:", "");
+					ProtectedRegion isChild = list.getValue().getParent();
+					
+					if(!owners.contains("timsandtoms") || !owners.contains("server") || owners == null || owners.isEmpty() || isChild == null){
+					
+						player.sendMessage("[InaciveRegions] It appears theres a region with a invalid owner name! -> Region Name: "+newList);
+						i++;
+						
+					}
+					
+					
+				}
+			}
+			
+			player.sendMessage(ChatColor.DARK_GRAY + "------------------------------------------------");
+			player.sendMessage(ChatColor.RED + "[ " + ChatColor.WHITE + i + ChatColor.RED + " ]" + ChatColor.RED +" reported invalid owner(s)");
+			player.sendMessage(ChatColor.DARK_GRAY + "------------------------------------------------");
+			invalidNames.clear();
+			return true;
+			
+			
+		}
+		
+		
+		
 		if(cmd.getName().equalsIgnoreCase("oldregions")){
 			
 			int i = 1;
@@ -101,25 +147,29 @@ public class main extends JavaPlugin  {
 									owners = list.getValue().getOwners().toPlayersString().trim().replace("name:", "");
 									owners = Bukkit.getServer().getOfflinePlayer(owners).getUniqueId().toString();
 								}
-								
-								long maxInactiveDays = 7776000000L; //7776000000L
 
+
+									
+							
+									long maxInactiveDays = 7776000000L; //7776000000L
+									OfflinePlayer getPlayer = Bukkit.getServer().getOfflinePlayer(UUID.fromString(owners));
+									if(ess3.getUser(getPlayer) != null){
+										
+									
+										long lastLogout = ess3.getUser(getPlayer).getLastLogout();	
+										long lastLogin = ess3.getUser(getPlayer).getLastLogin();	
+										if(lastLogout > lastLogin){
+											long difference = lastLogout+maxInactiveDays;
+											if(System.currentTimeMillis() > difference){
+												oNames.add(newList);
+											}					
+										
+										}
+									}
 								
-								OfflinePlayer getPlayer = Bukkit.getServer().getOfflinePlayer(UUID.fromString(owners));
-								long lastLogout = ess3.getUser(getPlayer).getLastLogout();	
-								long lastLogin = ess3.getUser(getPlayer).getLastLogin();	
-								if(lastLogout > lastLogin){
-									long difference = lastLogout+maxInactiveDays;
-									if(System.currentTimeMillis() > difference){
-										oNames.add(newList);
-									}					
 								
-								}
 							}
 						
-					}else{
-							Bukkit.getLogger().info("[InaciveRegions] It appears theres a region with a invalid owner name! -> Region Name: "+newList);
-							
 					}
 					
 					
